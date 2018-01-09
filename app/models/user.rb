f8fc64ed5b6acaf67f7888153c98a398c25e1d93
yonsuke <yonsuke99@gmail.com>
2018-01-10 00:41:15 +0900

    Implement registry form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum gender: { male:0, female:1 }
 end
