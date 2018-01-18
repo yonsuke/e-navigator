0741061e005df7b49f39944c9da10b9addeff07e
yonsuke <yonsuke99@gmail.com>
2018-01-18 14:11:59 +0900

    Create test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザー
+5.times do |n|
+  email = "#{n + 1}@test.com"
+  password = "password"
+  name  = "ユーザー#{n + 1}"
+  birthdate = "199#{n}-10-2#{n}"
+  gender = n % 2
+  school = "第#{n + 1}大学"
+  User.create!(email: email,
+               password: password,
+               name: name,
+               birthdate: birthdate,
+               gender: gender,
+               school: school)
+end
+
+# インタビュー
+users = User.all
+6.times do |n|
+  start_time = "2018-02-1#{n} 1#{n}:2#{n}:00"
+  users.each { |user| user.interviews.create!(start_time: start_time,
+                                              user_id: user.id) }
+end
