3056f9345835d13ebda794a5ccbe85b6aaf7be3c
yonsuke <yonsuke99@gmail.com>
2018-01-18 13:56:28 +0900

    Create interview

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :interviews
   validates :name,  presence: true
   validates :birthdate,  presence: true
   validates :gender,  presence: true
