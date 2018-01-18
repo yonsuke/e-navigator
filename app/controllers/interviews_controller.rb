6fa82e93690d11e3d5956bc2a2805069f8258f9c
yonsuke <yonsuke99@gmail.com>
2018-01-18 19:28:35 +0900

    Implement create interview

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,14 +1,29 @@
 class InterviewsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @user = current_user
     @interviews = current_user.interviews
   end
 
   def new
+    @interview = Interview.new
   end
 
   def create
+    @interview = current_user.interviews.build(interview_params)
+    if @interview.save
+      flash[:notice] = "面接候補日を追加しました"
+      redirect_to user_interviews_path
+    else
+
+    end
   end
+
+  private
+
+    def interview_params
+      params.require(:interview).permit(:start_time)
+    end
+
 end
