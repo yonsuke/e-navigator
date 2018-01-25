1f5cece8aa0defffc16c9e5df282d713db36f540
yonsuke <yonsuke99@gmail.com>
2018-01-26 01:13:07 +0900

    feat: Add show.html.erb for after create and update

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -13,8 +13,8 @@ class InterviewsController < ApplicationController
   def create
     @interview = current_user.interviews.build(interview_params)
     if @interview.save
-      flash[:notice] = "面談候補日を追加しました"
-      redirect_to user_interviews_path
+      flash.now[:notice] = "面談候補日を追加しました"
+      render 'show'
     else
       render 'new'
     end
@@ -29,16 +29,16 @@ class InterviewsController < ApplicationController
     user_id = @interview.user_id
     if user_id == current_user.id
       if @interview.update_attributes(interview_params)
-        flash[:notice] = "面談候補日を更新しました"
-        redirect_to user_interviews_path
+        flash.now[:notice] = "面談候補日を更新しました"
+        render 'show'
       else
         render 'edit'
       end
     else
       interviews = Interview.where(user_id: user_id).update_all(status: :rejected)
       @interview.approved!
-      flash[:notice] = "面談日を設定しました"
-      redirect_to user_interviews_path
+      flash.now[:notice] = "面談日を設定しました"
+      render 'show'
     end
   end
 
