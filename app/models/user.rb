4d01d5337cda994a208f79629d68c8bafad44b90
yonsuke <yonsuke99@gmail.com>
2018-01-30 13:16:05 +0900

    feat: Add default_scope to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :interviews
+  default_scope -> { order(:id) }
   validates :name,  presence: true
   validates :birthdate,  presence: true
   validates :gender,  presence: true
